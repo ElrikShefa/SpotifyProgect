e5bd17c1b2eb495237394bd8c4f957bd9f49755d
Matvey Chernyshov <elrik.shefa@gmail.com>
2021-05-23 18:28:35 +0300

    work with auth manager

--- Spotify/Spotify/Controllers/Other/AuthViewController.swift
+++ Spotify/Spotify/Controllers/Other/AuthViewController.swift
@@ -5,4 +5,61 @@
 //  Created by Матвей Чернышев on 21.05.2021.
 //
 
-import Foundation
+import UIKit
+import WebKit
+
+final class AuthViewController: UIViewController {
+
+	private let webView: WKWebView = {
+		let prefs = WKWebpagePreferences()
+		prefs.allowsContentJavaScript = true
+		let config = WKWebViewConfiguration()
+		let webView = WKWebView(frame: .zero, configuration: config)
+
+		return webView
+	}()
+
+	public var completionHandler: ((Bool) -> Void)?
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setupUI()
+		webViewLoad()
+	}
+
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		webView.frame = view.bounds
+	}
+
+	func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+		guard
+			let url = webView.url,
+			//Exchange the code for access token
+			let code = URLComponents(string: url.absoluteString)?
+				.queryItems?
+				.first(where: { $0.name == "code" })?
+				.value else { return}
+
+		print("\(code)")
+	}
+}
+
+extension AuthViewController: WKNavigationDelegate {}
+
+private extension AuthViewController {
+
+	func setupUI() {
+		title = "Sing In"
+
+		view.backgroundColor = .systemBackground
+		view.addSubview(webView)
+
+		webView.navigationDelegate = self
+	}
+
+	func webViewLoad() {
+		guard let url = AuthManager.shared.signInURL else { return }
+		webView.load(URLRequest(url: url))
+	}
+}
--- Spotify/Spotify/Managers/AuthManager.swift
+++ Spotify/Spotify/Managers/AuthManager.swift
@@ -11,6 +11,19 @@ final class AuthManager {
 
 	static let shared = AuthManager()
 
+	struct Constants {
+		static let clientID = "85f5bf43433948ad992793bec814b7f4"
+		static let clientSecret = "872fda6f02b8443a9681193d66d9f706"
+	}
+
+	public var signInURL: URL? {
+		let scopes = "user-read-private"
+		let redirectURL = "https://www.iosacademy.io"
+		let base = "https://accounts.spotify.com/authorize"
+		let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURL)&show_dialog=TRUE"
+		return URL(string: string)
+	}
+
 	private init() {}
 
 	var isSignedIn: Bool {
@@ -31,5 +44,20 @@ final class AuthManager {
 
 	private var shouldRefreshToken: Bool {
 		return false
+	}
+
+	public func exchangeCodeForToken(code: String, comlition: @escaping (Bool) -> Void) {
+		//Get token
+	}
+
+	public func refreshAccessToken() {
+
+	}
+}
+
+private extension AuthManager {
+
+	func cacheToken() {
+
 	}
 }
